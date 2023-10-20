0ddebc55d913edaab05797b7398e780898958efa
Evan Yang <evan.yang@hitobito.co.jp>
2023-10-20 14:41:07 +0800

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h2>About</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. In quis eligendi
+      voluptates aut necessitatibus! Animi accusantium nobis dolore quia
+      praesentium.
+    </p>
+    <div>{{ data }}</div>
+  </div>
+</template>
+
+<script setup>
+const { data } = await useFetch("/api/currency/GBP");
+</script>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+
+p {
+  margin: 20px 0;
+}
+</style>
